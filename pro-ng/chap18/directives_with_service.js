238fb3fed5777c62c083b293676c033b5a44a61d
ngworkshop <hello@ng-workshop.com>
2014-08-25 16:03:22 +0200

    changes

--- pro-ng/chap18/directives_with_service.js
+++ pro-ng/chap18/directives_with_service.js
@@ -4,6 +4,7 @@ angular.module("customDirectives", ["customServices"])
       scope: { counter: "=counter" },
       link: function (scope, element, attrs) {
         element.on("click", function (event) {
+          // Ici, on utilise le service :
           logService.log("Button click: " + event.target.innerText);
           scope.$apply(function () {
             scope.counter++;
