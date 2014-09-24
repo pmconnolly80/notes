b31862d689ee528b6cf356cc5a492b4647fafc1b
ngworkshop <hello@ng-workshop.com>
2014-09-24 11:56:53 +0200

    changes

---
+++ pro-ng/chap25/tests/filterTest.js
@@ -0,0 +1,22 @@
+describe("Filter Tests", function() {
+  var filterInstance;
+
+  beforeEach(angular.mock.module("exampleApp04"));
+
+  // Instancie le filtre programmatiquement.
+  beforeEach(angular.mock.inject(function($filter) {
+    filterInstance = $filter("labelCase");
+  }));
+
+  // Test le filtre instancié.
+  it("Changes case", function() {
+    var result = filterInstance("test phrase");
+    expect(result).toEqual("Test phrase");
+  });
+
+  it("Reverse case", function() {
+    var result = filterInstance("test phrase", true);
+    expect(result).toEqual("tEST PHRASE");
+  });
+
+});
